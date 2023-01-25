02ebb0b58170f53ca6903123730d08ff4dfb46ff
Javier López <javierpitino@gmail.com>
2023-01-25 13:23:34 +0100

    ajuste d epuntero

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 const FollowMouse = () => {
-  const [enabled, setEnabled] = useState(false);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
 
   // Pointer move effect
   useEffect(() => {
-    console.log("Efecto", { enabled });
+    // console.log('Efecto', { enabled })
     const handleMove = (event) => {
-      const { clientX, clientY } = event;
-      console.log("handleMove", { clientX, clientY });
-      setPosition({ x: clientX, y: clientY });
-    };
+      const { clientX, clientY } = event
+      // console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
     if (enabled) {
-      window.addEventListener("pointermove", handleMove);
+      window.addEventListener('pointermove', handleMove)
     }
 
     // Cleanup cuando el componente se desmonta
     // Cuadno cambian las dependencias ANTES de ejecutar el efecto de nuevo
     return () => {
-      console.log("Cleanup");
-      window.removeEventListener("pointermove", handleMove);
-    };
-  }, [enabled]);
+      // console.log('Cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   // [] -> Solo se ejecuta una vez cuando de monta el componente
   // [dependencia] -> Se ejecuta cuando cambia la dependencia y cuando se monta el componente
@@ -30,12 +30,12 @@ const FollowMouse = () => {
 
   // Change body className
   useEffect(() => {
-    document.body.classList.toggle("no-cursor", enabled);
+    document.body.classList.toggle('no-cursor', enabled)
 
     return () => {
-      document.body.classList.remove("no-cursor");
-    };
-  }, [enabled]);
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
 
   return (
     <>
@@ -48,11 +48,11 @@ const FollowMouse = () => {
             borderRadius: '50%',
             opacity: 0.3,
             pointerEvents: 'none',
-            left: -25,
-            top: -25,
+            left: -15,
+            top: -15,
             width: 30,
             height: 30,
-            transform: `translate(${position.x}px, ${position.y}px)`,
+            transform: `translate(${position.x}px, ${position.y}px)`
           }}
         />
       )}
@@ -60,15 +60,15 @@ const FollowMouse = () => {
         {enabled ? 'Desactivar' : 'Activar'} Seguir puntero
       </button>
     </>
-  );
-};
+  )
+}
 
-function App() {
+function App () {
   return (
     <main>
       <FollowMouse />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
